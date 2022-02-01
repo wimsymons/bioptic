cd330097b522d12be24a2f7004dd27cf17dce564
Wim Symons <wim.symons@vrt.be>
2022-02-01 11:46:37 +0100

    Added 'Plaats' option (random, centered) Fixed button colors to look like noUISlider Fixed 'Snelheid' slider (shows seconds), no longer automatically starts/stops Fixed bottom whitespace below noUISliders Fixed location on screen, so it never falls off

--- js/main.js
+++ js/main.js
@@ -1,27 +1,37 @@
+'use strict';
+
 // constants
-const defaultSpeed = 5;
-const maxSpeed = 20;
-const speedMultiplier = 100;
+const speedMin = 2;
+const speedMax = 10;
+const speedStep = 1;
+const initialSpeed = 5;
+const speedMultiplier = 1000;
+
+const fontSizeMin = 0;
+const fontSizeMax = 100;
+const fontSizeStep = 5;
+const initialFontSizeRange = [30, 70];
 
 // variables
 var intervalId = null;
-var fontSizeRange = [30, 70];
-var speed = defaultSpeed;
+var fontSizeRange = initialFontSizeRange;
+var speed = initialSpeed;
 var textColor = "#000000";
 var slideDirection = 'L';
+var contentLocation = 'R';
 var words;
 
 // on load: grootte + snelheid slider
 document.addEventListener('DOMContentLoaded', function () {
   var fontSizeSlider = document.getElementById('grootte');
   noUiSlider.create(fontSizeSlider, {
-    start: fontSizeRange,
+    start: initialFontSizeRange,
     connect: true,
     range: {
-      'min': 10,
-      'max': 100
+      'min': fontSizeMin,
+      'max': fontSizeMax
     },
-    step: 5,
+    step: fontSizeStep,
     format: {
       from: function (value) {
         return Number(parseInt(value));
@@ -40,47 +50,44 @@ document.addEventListener('DOMContentLoaded', function () {
 
   var speedSlider = document.getElementById('snelheid');
   noUiSlider.create(speedSlider, {
-    start: defaultSpeed,
+    start: initialSpeed,
     connect: 'lower',
     range: {
-      'min': 0,
-      'max': 10
+      'min': speedMin,
+      'max': speedMax
     },
-    step: 1,
+    step: speedStep,
     format: {
       from: function (value) {
         return Number(parseInt(value));
       },
       to: function (value) {
-        return parseInt(value).toString();
+        return parseInt(value) + 's';
       }
     },
     tooltips: true
   });
   speedSlider.noUiSlider.on('change', function (values) {
-    speed = [parseInt(values[0])];
+    speed = parseInt(values[0]);
     if (intervalId) {
+      // when started, stop first and re-schedule
       clearInterval(intervalId);
       intervalId = null;
-    }
-    var milliseconds = (maxSpeed - speed) * speedMultiplier;
-    console.log("Speed", speed, ",", milliseconds, "ms");
-    if (speed > 0) {
+      changeElement(); // show first immediately
+      var milliseconds = calculateInterval(speed);
+      console.log("Showing every", milliseconds, "ms");
       intervalId = setInterval(changeElement, milliseconds);
-      document.getElementById('start').classList.add("started");
-      document.getElementById('stop').classList.remove("stopped");
-    } else {
-      document.getElementById('start').classList.remove("started");
-      document.getElementById('stop').classList.add("stopped");
     }
   });
 
-  loadWords()
+  loadWords();
 });
 
 // sidebar open-close
 document.getElementById('openclose').addEventListener('click', function () {
   document.getElementById('sidebar').classList.toggle("sidebar-closed");
+  // re-draw
+  changeElement();
 });
 
 // inhoud
@@ -99,6 +106,11 @@ document.getElementById('achtergrond').addEventListener('change', function () {
   document.getElementById('playground').style = 'background-color:' + document.getElementById('achtergrond').value;
 });
 
+// plaats: C(entered), R(andom)
+document.getElementById('plaats').addEventListener('change', function () {
+  contentLocation = document.getElementById('plaats').value;
+});
+
 // richting: L(eft), R(ight), D(own), U(p), A(ll)
 document.getElementById('richting').addEventListener('change', function () {
   slideDirection = document.getElementById('richting').value;
@@ -106,49 +118,68 @@ document.getElementById('richting').addEventListener('change', function () {
 
 // start
 document.getElementById('start').addEventListener('click', function () {
-  if (speed > 0) {
+  if (intervalId) {
+    // when started, stop first
+    clearInterval(intervalId);
+    intervalId = null;
+  } else {
     document.getElementById('start').classList.add("started");
     document.getElementById('stop').classList.remove("stopped");
   }
-  if (!intervalId && speed > 0) {
-    var milliseconds = (maxSpeed - speed) * speedMultiplier;
-    console.log("Speed", speed, ",", milliseconds, "ms");
-    intervalId = setInterval(changeElement, milliseconds);
-  } else {
-    // show once
-    changeElement();
-  }
+  // start again
+  var milliseconds = calculateInterval(speed);
+  console.log("Started, show every", milliseconds, "ms");
+  changeElement(); // show first immediately
+  intervalId = setInterval(changeElement, milliseconds);
 });
 
 // stop
 document.getElementById('stop').addEventListener('click', function () {
-  document.getElementById('start').classList.remove("started");
-  document.getElementById('stop').classList.add("stopped");
-  clearInterval(intervalId);
-  intervalId = null;
+  if (intervalId) {
+    document.getElementById('start').classList.remove("started");
+    document.getElementById('stop').classList.add("stopped");
+    clearInterval(intervalId);
+    intervalId = null;
+    console.log("Stopped");
+  }
 });
 
 function changeElement() {
   var elem = document.getElementById('element');
   let playGroundElem = document.getElementById('playground');
 
-  var randomFontSize = fontSizeRange[0] + Math.floor(Math.random() * (fontSizeRange[1] - fontSizeRange[0]));
+  var fontSize = fontSizeRange[0] + Math.floor(Math.random() * (fontSizeRange[1] - fontSizeRange[0]));
 
-  var randomX = Math.floor(Math.random() * playGroundElem.clientWidth);
-  var randomY = Math.floor(Math.random() * playGroundElem.clientHeight);
-
-  console.log("x = ", randomX, " y = ", randomX, " size = ", randomFontSize)
-
-  // zet kleur, grootte, locatie
-  elem.style = 'color:' + textColor + ';font-size:' + randomFontSize + 'px;top:' + randomY + 'px;left:' + randomX + 'px';
-
+  // give new text
   setText(elem);
+
+  // set color and font size to recalc elem width/height
+  elem.style.color = textColor;
+  elem.style.fontSize = fontSize + 'px';
+
+  var x, y;
+  if (contentLocation === 'C') {
+    x = Math.floor(0.5 * (playGroundElem.clientWidth - elem.clientWidth));
+    y = Math.floor(0.5 * (playGroundElem.clientHeight - elem.clientHeight));
+  } else if (contentLocation === 'R') {
+    x = Math.floor(Math.random() * (playGroundElem.clientWidth - elem.clientWidth));
+    y = Math.floor(Math.random() * (playGroundElem.clientHeight - elem.clientHeight));
+
+  }
+  elem.style.top = y + 'px';
+  elem.style.left = x + 'px';
 }
 
 function setText(element) {
   if (words) {
-    element.innerText = words[Math.floor(Math.random() * words.length)];
+    var newWord = words[Math.floor(Math.random() * words.length)];
+    console.log("Showing", newWord);
+    element.innerText = newWord;
   }
+}
+
+function calculateInterval(newSpeed) {
+  return newSpeed * speedMultiplier;
 }
 
 function loadWords() {
@@ -156,8 +187,9 @@ function loadWords() {
   console.log("Loading words", wordFile);
   fetch(wordFile).then(function (response) {
     response.text().then(function (text) {
-      words = text.split('\n');
+      words = text.split('\n').filter(word => word);
       console.log("Loaded", words.length, "words");
+      changeElement();
     });
   });
 }
